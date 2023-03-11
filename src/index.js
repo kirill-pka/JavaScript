f6ea95b6f1381a8680cc9c1f376e9052590d0de8
classigone@gmail.com <classigone@gmail.com>
2023-03-11 23:19:09 +0200

    Node

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import validatePasword from './js/Node/validate';
+console.log(validatePasword('qweqwe'));
+
+import { addUser } from './js/Node/api-servise';
+addUser('mango');
+
+// import {
+//     fetchAllUsers,
+//     fetchUsersById,
+//     updateUsersById,
+//     x as value,
+//     y as name,
+// } from './js/Node/api-servise';
+
+// console.log(fetchAllUsers);
+// console.log(fetchUsersById);
+// console.log(updateUsersById);
+// console.log(value);
+// console.log(name);
+
+// import * as apiService from './js/Node/api-servise';
+
+// console.log(apiService);
+
