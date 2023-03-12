5f708e359f7e32ef77c1d3a63296855586204a7a
classigone@gmail.com <classigone@gmail.com>
2023-03-12 11:23:19 +0200

    webpack

--- src/index.js
+++ src/index.js
@@ -1,24 +1,4 @@
-import validatePasword from './js/Node/validate';
-console.log(validatePasword('qweqwe'));
+import name from './module-1';
 
-import { addUser } from './js/Node/api-servise';
-addUser('mango');
-
-// import {
-//     fetchAllUsers,
-//     fetchUsersById,
-//     updateUsersById,
-//     x as value,
-//     y as name,
-// } from './js/Node/api-servise';
-
-// console.log(fetchAllUsers);
-// console.log(fetchUsersById);
-// console.log(updateUsersById);
-// console.log(value);
-// console.log(name);
-
-// import * as apiService from './js/Node/api-servise';
-
-// console.log(apiService);
-
+console.log(name)
+console.log('Это index.js');
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,9 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'bundle.js',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: ['babel-loader']
-            },
-        ],
-    },
-    devServer: {
-        open: true,
-    },
+  entry: './src/index.js',
+  output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: 'bundle.js',
+  },
 };
